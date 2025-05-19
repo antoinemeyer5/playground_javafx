fce644c6c25a1dd10008c1ec6de0d623720c4040
antoinemeyer5 <antoine.meyer54@gmail.com>
2025-05-19 12:56:22 +0200

    Fix execute script

--- execute.sh
+++ execute.sh
@@ -7,7 +7,7 @@ compile="javac
     --module-path $PATH_TO_FX
     --add-modules javafx.controls
     -d $DIR_OUTPUT/
-    $DIR_SOURCE/HelloFX.java"
+    $DIR_SOURCE/*.java"
 
 run="java
     --module-path $PATH_TO_FX
